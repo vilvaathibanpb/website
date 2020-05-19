7d74ace38f8ef92aad343ca8d313d8cbcc631465
vilvaathibanpb <vilvaathiban@gmail.com>
2020-05-19 09:40:26 +0200

    Home section

--- src/App.js
+++ src/App.js
@@ -1,85 +1,21 @@
-import React from "react";
-import styled from "styled-components";
-import logo from "./assets/logo.png";
-import image from "./assets/img.png";
-import linkedin from "./assets/linkedin.png";
-import medium from "./assets/medium.png";
-
-const Container = styled.div`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  padding: 30px;
-  box-sizing: border-box;
-`;
-
-const Image = styled.img`
-  width: 18px;
-  height: 18px;
-`;
-
-const Icon = styled.div`
-  border: 1px solid rgba(0, 0, 0, 0.4);
-  border-radius: 50%;
-  padding: 5px;
-  margin-left: 30px;
-  margin-top: 40px;
-`;
-
-const H3 = styled.div`
-  font-family: SF Pro Display;
-  font-style: normal;
-  font-weight: bold;
-  font-size: 2rem;
-  line-height: 48px;
-  text-align: center;
-  color: #252c54;
-  margin-top: 5vh;
-`;
-
-const P = styled.p`
-  position: fixed;
-  bottom: 0;
-  font-family: SF Pro Display;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 1rem;
-  line-height: 24px;
-  text-align: center;
-  color: #252c54;
-`;
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
+  const [theme, setTheme] = useState("light");
+
+  const toggle = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
   return (
-    <Container>
-      <div style={{ marginBottom: "7vh" }}>
-        <img src={logo} alt="product ride logo" />
+    <div className={theme}>
+      <div className=" bg-main min-h-screen">
+        <Header theme={theme} toggle={() => toggle()} />
+        <Home theme={theme} />
       </div>
-      <div>
-        <img src={image} alt="Launching soon" />
-      </div>
-      <div>
-        <H3>Buckle up. We are preparing for launch.</H3>
-      </div>
-      <div style={{ display: "flex", justifyContent: "center", width: "100%" }}>
-        <a href="https://medium.com/product-ride" target="_blank">
-          <Icon>
-            <Image src={medium} alt="product ride medium" />
-          </Icon>
-        </a>
-        <a href="https://www.linkedin.com/company/product-ride" target="_blank">
-          <Icon>
-            <Image src={linkedin} alt="product ride linkedin" />
-          </Icon>
-        </a>
-      </div>
-      <P>
-        Being made with <span style={{ color: "#FF4133" }}>❤</span> ️ in Germany
-      </P>
-    </Container>
+    </div>
   );
 }
 
---
+++ src/components/Button/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Button = ({title}) => {
+return <button className="rounded-md bg-secondary text-white py-5 px-20 mt-6 text-base font-bold box-border">{title}</button>
+}
+export default Button
