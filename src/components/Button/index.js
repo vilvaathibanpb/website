d44edbe9cc3236b5bab8cfac3894f78e17873ed2
vilvaathibanpb <vilvaathiban@gmail.com>
2020-05-31 11:41:57 +0200

    fix: scroll and form

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Button = ({title}) => {
-return <button className="rounded-md bg-secondary text-white py-5 px-20 mt-6 text-base font-bold box-border">{title}</button>
+const Button = ({title, onClick}) => {
+return <button onClick={onClick ? onClick : () => {}} className="rounded-md bg-secondary text-white py-5 px-20 mt-6 text-base font-bold box-border">{title}</button>
 }
 export default Button
